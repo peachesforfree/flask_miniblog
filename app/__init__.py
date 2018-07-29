a834fa59b39f719564c505f8b3557325a76d6c37
Sergio Balcorta <sergio.shearing@gmail.com>
2018-07-28 19:11:53 -0700

    first commit and left off at seciton 7 lecture 40

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+login = LoginManager(app)
+migrate = Migrate(app, db)
+login.login_view = 'login'
+
+from app import routes, models
