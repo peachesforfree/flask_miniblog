7abed925a843e22176d715acd5b02c0c61410932
Sergio Balcorta <sergio.shearing@gmail.com>
2018-07-29 12:27:03 -0700

    start off at lecture 51

--- app/models.py
+++ app/models.py
@@ -11,6 +11,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -21,9 +23,9 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def avater(self, size):
+    def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravater.com/avatar/{}?d=identicon&s={}'.format(digest,size)
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest,size)
 
 @login.user_loader
 def load_user(id):
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
+from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db	#importing app variable from the package, as defined in init__.py
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
+
+@app.before_request
+def before_request():					#invoked by flask before all routes
+	if current_user.is_authenticated:
+		current_user.last_seen = datetime.utcnow()
+		db.session.commit()
+
 
 @app.route('/')
 @app.route('/index')
@@ -20,6 +28,7 @@ def index():
 		}
 	]
 	return render_template('index.html', title='Home', posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -39,10 +48,12 @@ def login():
 		return redirect(url_for('index'))
 	return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 def logout():
 	logout_user()
 	return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -58,6 +69,7 @@ def register():
 		return redirect(url_for('login'))
 	return render_template('register.html', title='Register', form=form)
 
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
@@ -67,3 +79,19 @@ def user(username):
 		{'author': user, 'body': 'Test post #2'}
 	]
 	return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+	form = EditProfileForm()
+	if form.validate_on_submit():
+		current_user.username = form.username.data
+		current_user.about_me = form.about_me.data
+		db.session.commit()
+		flash('Your changes have be saved.')
+		return redirect(url_for('edit_profile'))
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.about_me.data = current_user.about_me
+	return render_template('edit_profile.html', title='Edit Profile', form=form)
